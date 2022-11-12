f21350aabf58a2053d4b7ebe7577b21e0c211bd4
sherusaru10 <sherusaru07@gmail.com>
2022-11-12 08:34:06 +0545

    FormHandling

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -9,11 +9,11 @@ const Expense = () => {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { 
-      id: "e2", 
-      title: "New TV", 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
     },
     {
       id: "e3",
@@ -31,27 +31,27 @@ const Expense = () => {
   return (
     <div>
       <NewExpense />
-          <ExpenseItem
-            title={expenses[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={expenses[3].title}
-            amount={expenses[3].amount}
-            date={expenses[3].date}
-          ></ExpenseItem>
-        </div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
   );
 };
 
